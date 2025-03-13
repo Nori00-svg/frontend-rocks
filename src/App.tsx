9e324ceaac7f64487c85c0c988b391154abf8859
NOR <norstore07@gmail.com>
2025-03-13 11:30:53 +0000

    f

--- src/App.tsx
+++ src/App.tsx
@@ -29,14 +29,14 @@ export const App = () => {
 
         <div className="flex flex-col items-center space-y-4">
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
+            className="bg-violet-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
             onClick={() => setCount((count) => count + 9)}
           >
             Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
           </button>
 
           <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
+              className="bg-violet-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
               onClick={() => setTitle("Charizard")}
           >
             Cambia
